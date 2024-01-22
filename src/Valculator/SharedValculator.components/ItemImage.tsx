8a463faf57011e31e3d02f6858761eb049dfcab2
charlotte-hues <hues.charlotte@gmail.com>
2024-01-22 18:35:07 +0000

    item images

--- src/Valculator/SharedValculator.components/ItemImage.tsx
+++ src/Valculator/SharedValculator.components/ItemImage.tsx
@@ -1,22 +1,19 @@
-// import Image from "next/image";
-
 import { IItem } from "../data/@types/ValheimData.types";
-// import { getItemImageSrc } from "../Valculator.helpers";
+import { getItemImageSrc } from "../Valculator.helpers";
 
 export const ItemImage = ({
   item,
-  // size = 60,
+  size = 60,
 }: {
   item: IItem;
   size?: number;
 }) => {
   return (
-    <div>{item.name}</div>
-    // <Image
-    //   width={size}
-    //   height={size}
-    //   src={getItemImageSrc(item)}
-    //   alt={`${item.set} ${item.type ? item.type : item.name}`}
-    // />
+    <img
+      width={size}
+      height={size}
+      src={getItemImageSrc(item)}
+      alt={`${item.set} ${item.type ? item.type : item.name}`}
+    />
   );
 };
