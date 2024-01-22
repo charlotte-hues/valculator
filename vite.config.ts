46619a27b038471849496d31dc4e3f69fb3f79a0
charlotte-hues <hues.charlotte@gmail.com>
2024-01-22 23:15:31 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+import typescript from "@rollup/plugin-typescript";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { typescriptPaths } from "rollup-plugin-typescript-paths";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -8,6 +10,30 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
+    },
+  },
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      name: "valculator",
+      formats: ["es", "umd"],
+      entry: path.resolve(__dirname, "src/main.tsx"),
+      fileName: "valculator",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }) as Plugin,
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: "dist",
+        }) as Plugin,
+      ],
     },
   },
 });
