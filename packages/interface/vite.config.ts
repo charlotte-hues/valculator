702badca26aadbfc48dedd9aeb2b9ad1ceebb0f3
charlotte-hues <hues.charlotte@gmail.com>
2024-02-18 02:01:20 +0000

    manual chunks

--- packages/interface/vite.config.ts
+++ packages/interface/vite.config.ts
@@ -1,7 +1,35 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        interface: path.join(__dirname, "src/main.ts"),
+      },
+      output: {
+        entryFileNames: "entry-[name].js",
+        manualChunks: (id) => {
+          console.log(id);
+
+          if (id.includes(".data.ts")) {
+            return "valculator_data";
+          }
+          if (id.includes(".png")) {
+            return "valculator_images";
+          }
+          if (id.includes("node_modules")) {
+            if (id.includes("@material-ui")) {
+              return "vendor_mui";
+            }
+
+            return "vendor"; // all other package goes here
+          }
+        },
+      },
+    },
+  },
 });
