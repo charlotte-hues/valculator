6cd2b004d2b4aaad0f05d01d5a5fb69d9f11fe98
charlotte-hues <hues.charlotte@gmail.com>
2024-02-18 00:34:14 +0000

    move images into interface

--- packages/interface/src/helpers/getItemImageSrc.ts
+++ packages/interface/src/helpers/getItemImageSrc.ts
@@ -5,7 +5,7 @@ export const getItemImageSrc = (item: IItem) => {
   const isMeadBase = item.set === "mead" && item.type == "base";
 
   return new URL(
-    `./data/images/item-images/${isMeadBase ? "recipes" : item.group}/${
+    `../images/item-images/${isMeadBase ? "recipes" : item.group}/${
       isMeadBase ? "MeadBase" : removeSpaces(capitalize(item.name))
     }.png`,
     import.meta.url
