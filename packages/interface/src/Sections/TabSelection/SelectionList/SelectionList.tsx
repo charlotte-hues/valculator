8e8a8fb9979c9e41a9e5a0bd6a06c6928b8aeb1b
charlotte-hues <hues.charlotte@gmail.com>
2024-02-29 15:12:21 +0000

    fix: always render selection groups [HEL-124]

--- packages/interface/src/Sections/TabSelection/SelectionList/SelectionList.tsx
+++ packages/interface/src/Sections/TabSelection/SelectionList/SelectionList.tsx
@@ -1,13 +1,10 @@
 import { Stack } from "@mui/material";
-import RenderIfVisible from "react-render-if-visible";
 
 import { GroupType } from "@valculator/data/types";
 import { EmptyListText } from "../../../components/shared/EmptyListText";
 
 import { useTabSelectionContext } from "../TabSelection.context";
 import { SelectionGroup } from "./SelectionList.components";
-
-const ESTIMATED_HEIGHT = 600;
 
 export const SelectionList = () => {
   const { groups } = useTabSelectionContext();
@@ -18,12 +15,7 @@ export const SelectionList = () => {
         const groupName = Object.keys(group)[0] as GroupType;
 
         return (
-          <RenderIfVisible key={groupName} defaultHeight={ESTIMATED_HEIGHT}>
-            <SelectionGroup
-              groupItems={group[groupName]}
-              groupName={groupName}
-            />
-          </RenderIfVisible>
+          <SelectionGroup groupItems={group[groupName]} groupName={groupName} />
         );
       })}
       {groups.length === 0 && (
